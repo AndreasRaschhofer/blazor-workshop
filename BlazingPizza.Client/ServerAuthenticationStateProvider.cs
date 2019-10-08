238a2d3eb968c70f01ed790845c2fe66643109d1
Andreas Raschhofer <andreas.raschhofer@gmail.com>
2019-10-08 17:13:34 +0200

    s06 basic authentication (with fake user)

---
+++ BlazingPizza.Client/ServerAuthenticationStateProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Client
+{
+    public class ServerAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            // Currently, this returns fake data
+            // In a moment, we'll get real data from the server
+            var claim = new Claim(ClaimTypes.Name, "Faky");
+            var identity = new ClaimsIdentity(new[] { claim }, "serverauth");
+            return new AuthenticationState(new ClaimsPrincipal(identity));
+        }
+    }
+}
